ee45b07f38096456b1903499aa3d7254a3f495a8
MUHAMMED FAYAZ T S <muhammedfayazts01@gmail.com>
2026-01-05 16:52:48 +0530

    feat: add zod validation and error handler

--- src/middlewares/middlewares.ts
+++ src/middlewares/middlewares.ts
@@ -2,7 +2,11 @@ import type { NextFunction, Request, Response } from "express";
 
 import type ErrorResponse from "../interfaces/error-response.js";
 
+import { HTTPSTATUS } from "../constants/httpstatus.js";
 import { env } from "../utils/env.js";
+import { formatZodError } from "../utils/zod.helper.js";
+// eslint-disable-next-line perfectionist/sort-imports
+import { ZodError } from "zod";
 
 export function notFound(req: Request, res: Response, next: NextFunction) {
   res.status(404);
@@ -16,6 +20,17 @@ export function errorHandler(
   res: Response<ErrorResponse>,
   _next: NextFunction
 ) {
+
+  if (err instanceof SyntaxError) {
+    return res.status(HTTPSTATUS.BAD_REQUEST).json({
+      message: 'Invalid JSON format, please check your request body',
+    });
+  }
+
+  if (err instanceof ZodError) {
+    return formatZodError(res, err);
+  }
+
   const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
   res.status(statusCode);
   res.json({
